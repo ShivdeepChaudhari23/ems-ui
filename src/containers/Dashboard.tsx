b024c12e0234cba601fc3c3b81fe2db7f0ed906b
ShivdeepChaudhari23 <shivdeepchaudhari23@gmail.com>
2024-07-18 20:38:57 +0530

    Categories table - Integrated getCategories API - Created table - Installed datagrid library from material ui

--- src/containers/Dashboard.tsx
+++ src/containers/Dashboard.tsx
@@ -1,126 +1,46 @@
-import * as React from 'react';
+import { useEffect, useState } from 'react';
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
 import { useNavigate } from 'react-router-dom';
+import { Container, Tab, Tabs } from '@mui/material';
 
-const pages = ['employees', 'categories'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+const pages = ['employees', 'categories', 'roles'];
+// const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 const  Dashboard: React.FC<React.PropsWithChildren> = ({ children }) => {
   const navigate = useNavigate();
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
-  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+  const [selectedTab, setSelectedTab] = useState('');
 
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
-  };
+  useEffect(() => {
+    const currentSegment = window.location.pathname.split('/')[1];
+    setSelectedTab(currentSegment);
+  }, []);
 
-  const handleCloseNavMenu = (page: string) => {
-    setAnchorElNav(null);
-    navigate(`/${page}`);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
+  const handleChangeTab = (tabName: string) => {
+    setSelectedTab(tabName);
+    navigate(`/${tabName}`);
+  }
 
   return (
     <div>
-        <AppBar position="static" className='bg-primaryBlue'>
+        <AppBar position="static" className='!bg-primaryBlue'>
             <Container maxWidth="xl">
                 <Toolbar disableGutters >
-                
-                <img src={'/Byteridge-EMS-Logo.svg'} className="w-[10%] max-h-[80px] mr-10" />
-                <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                    <IconButton
-                    size="large"
-                    aria-label="account of current user"
-                    aria-controls="menu-appbar"
-                    aria-haspopup="true"
-                    onClick={handleOpenNavMenu}
-                    color="inherit"
+                    <img src={'/Byteridge-EMS-Logo.svg'} className="w-[10%] max-h-[80px] mr-10" />
+                    <Tabs
+                        value={selectedTab}
+                        className='h-full'
                     >
-                    <MenuIcon />
-                    </IconButton>
-                    <Menu
-                    id="menu-appbar"
-                    anchorEl={anchorElNav}
-                    anchorOrigin={{
-                        vertical: 'bottom',
-                        horizontal: 'left',
-                    }}
-                    keepMounted
-                    transformOrigin={{
-                        vertical: 'top',
-                        horizontal: 'left',
-                    }}
-                    open={Boolean(anchorElNav)}
-                    onClose={handleCloseNavMenu}
-                    sx={{
-                        display: { xs: 'block', md: 'none' },
-                    }}
-                    >
-                    {pages.map((page) => (
-                        <MenuItem key={page} onClick={() => handleCloseNavMenu(page)}>
-                        <Typography textAlign="center">{page}</Typography>
-                        </MenuItem>
-                    ))}
-                    </Menu>
-                </Box>
-                <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                    {pages.map((page) => (
-                    <Button
-                        key={page}
-                        onClick={() => handleCloseNavMenu(page)}
-                        sx={{ my: 2, color: 'white', display: 'block' }}
-                    >
-                        {page}
-                    </Button>
-                    ))}
-                </Box>
-
-                <Box sx={{ flexGrow: 0 }}>
-                    <Tooltip title="Open settings">
-                    <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                        <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-                    </IconButton>
-                    </Tooltip>
-                    <Menu
-                    sx={{ mt: '45px' }}
-                    id="menu-appbar"
-                    anchorEl={anchorElUser}
-                    anchorOrigin={{
-                        vertical: 'top',
-                        horizontal: 'right',
-                    }}
-                    keepMounted
-                    transformOrigin={{
-                        vertical: 'top',
-                        horizontal: 'right',
-                    }}
-                    open={Boolean(anchorElUser)}
-                    onClose={handleCloseUserMenu}
-                    >
-                    {settings.map((setting) => (
-                        <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                        <Typography textAlign="center">{setting}</Typography>
-                        </MenuItem>
-                    ))}
-                    </Menu>
-                </Box>
+                        {pages.map((item) => {
+                            return (
+                                <Tab
+                                    label={item}
+                                    onClick={() => handleChangeTab(item)}
+                                    className={`${selectedTab === item ? 'selectedTab' : 'unselectedTab'} !font-bold !text-${selectedTab === item ? 'primaryBlue' : 'tabText'}`}
+                                />
+                            );
+                        })}
+                    </Tabs>
                 </Toolbar>
             </Container>
         </AppBar>
--- tailwindColors.ts
+++ tailwindColors.ts
@@ -1,4 +1,5 @@
 export const tailwindColors = {
     primaryBlue: "#2a39ab",
     secondaryBlue: "#98ace3",
+    tabText: "#34ebc9",
 };
